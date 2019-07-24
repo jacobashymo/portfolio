229c5c7a1b7568d6af65df16bc13ee7e564b6512
Jacob Moore <jacobashymo@gmail.com>
2019-07-23 23:06:58 -0500

    initial commit

---
+++ src/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="jumbotron">
+      <h1 className="display-4">About Jacob Moore</h1>
+      <p className="lead">
+        First and foremost I'm a <strong>Christ-follower</strong>. But I also
+        make my living in web development. I've worked primarily in React.js,
+        but I'm a quick learner and will pick up anything you throw at me.
+      </p>
+      <hr className="my-4" />
+      <p>
+        I also enjoy basketball, golf, and drumming. I've been doing all three
+        since I was a kid and will be more than happy to fill any needs you have
+        in those areas as well.
+      </p>
+      <a className="btn btn-primary btn-lg" href="/" role="button">
+        Contact Me
+      </a>
+    </div>
+  );
+};
+
+export default About;
